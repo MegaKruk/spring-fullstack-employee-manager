6bc6639fc714153e895bf76ff31fceef23c00497
MegaKruk <filip.mazur.dev@gmail.com>
2021-09-23 22:47:01 +0200

    Initial commit; Most basic functionality done

---
+++ src/main/java/com/megakruk/employeemanager/repository/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.megakruk.employeemanager.repository;
+
+import com.megakruk.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
